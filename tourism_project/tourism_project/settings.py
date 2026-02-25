df3ad2788139796a18d0ea4af4c567ac0f94d74b
Bishal Acharya <117291450+bishalacharya05@users.noreply.github.com>
2026-02-25 19:22:42 +0545

    Update ALLOWED_HOSTS in settings.py

--- tourism_project/tourism_project/settings.py
+++ tourism_project/tourism_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a1w3cq2=s2m7ff!klkearvnv&tda2d35fr1u3l3dh*lakb+&bd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['bishalacharyalab04.azurewebsites.net']
 
 
 # Application definition
@@ -123,3 +123,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
